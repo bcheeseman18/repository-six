829cbf52628a8685a3b0702156d752d17c8df24a
bcheeseman18 <benjamincheeseman18@gmail.com>
2017-04-05 14:29:41 -0400

    javascript one

---
+++ yourfile.js
@@ -0,0 +1,24 @@
+
+// HANGMAN LITE
+/* 
+Create a variable called word that contains a hangman word,
+ and create a second variable called letter that indicates the letter that the player guessed.
+ How many times does a letter exist 
+  - Print out "Yeah, the letter x exists y times in my word" if the letter exists (but substitute the actual letter and the number of times)
+  - If the letter doesn't exist, print out "Nope, that letter doesn't exist in my word."
+
+  input: creature
+  output: guessed letter
+
+  */
+
+  let word = creature
+  let letter = word[0]
+
+  for (count = 0; count < word.length; count = count + 1); {
+    if (word[count] === letter {
+        console.log(Yea, the letter is there);
+    } 
+
+
+}
